4aa38e2c571cd6bd4e0f9643e37161340a1ff04c
Andrii Piatakha <andrii.piatakha@travisperkins.co.uk>
2018-03-17 14:37:24 +0200

    commit

--- src/com/piatakha/lectures/creationalpatterns/abstractfactory/squadbuilder/elfsquade/ElfMage.java
+++
@@ -1,8 +0,0 @@
-package com.piatakha.lectures.creationalpatterns.abstractfactory.squadbuilder.elfsquade;
-
-import patterns.creationalpatterns.abstractfactory.squadbuilder.Mage;
-
-public class ElfMage implements Mage {
-	public void cast() {
-	}
-}
