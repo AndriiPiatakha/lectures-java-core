16a3f8d4a2822e61e216d016aec57ba2b81b6f52
Andrii Piatakha <andrii.piatakha@travisperkins.co.uk>
2018-03-23 15:01:34 +0200

    commit

--- src/generic/GenericDemo.java
+++ src/generic/GenericDemo.java
@@ -1,64 +1,79 @@
-//package com.piatakha.generic;
-//
-//import java.util.ArrayList;
-//import java.util.Arrays;
-//import java.util.List;
-//import java.util.function.IntFunction;
-//
-//public class GenericDemo {
-//	
-//	public static void main(String[] args) {
-//		Z<Parent> z = new Z<>(new Child());
-//		
-//		
-//		
-////		List<Integer> listInteger = new ArrayList<>();
-////		List list2 = listInteger;
-////		List<String> listString = list2;
-////		listString.add("asd");
-//	
-//		List<Child> list = new ArrayList<>();
-//		
-//		
-//		
-////		someMethod(list);
-//		String[] arr =  {"as", "s"};
-//		String[] array = Arrays.stream(arr).filter((str) -> str.length() < 1).toArray(String[]::new);
+package generic;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.IntFunction;
+
+public class GenericDemo {
+	
+	public static void main(String[] args) {
+		Z<Parent> z = new Z<>(new Child());
+		
+		
+		
+//		List<Integer> listInteger = new ArrayList<>();
+//		List list2 = listInteger;
+//		List<String> listString = list2;
+//		listString.add("asd");
+	
+		List<Child> list = new ArrayList<>();
+		
+		
+		// can call only if generic
+//		someMethod(list);
+		String[] arr =  {"as", "s"};
+		String[] array = Arrays.stream(arr).filter((str) -> str.length() < 1).toArray(String[]::new);
+	
+	}
+	
+	
+	// It could be List<Child> here so I can't add Parent to it
+//	public static void someMethod(List<? extends Parent> list) {
+//		list.add(new Child());
 //	}
-//	
-//	
-////	public static void someMethod(List<? extends Parent> list) {
-////		list.add(new Child());
-////	}
-//
-//}
-//
-//class Z<T extends Parent> {
-//	
-//	private T someField;
-//	
-//	public Z(T t) {
-//		this.someField = t;
+	
+//	public static void someMethod2(List<? super Child> list) {
+//		list.add(new Object());
+//		list.add(new Child());
+//		list.add(new Child2());
+//		list.add(new Parent());
+//		
+//		Object child = list.get(1);
 //	}
-//	
-//	public T getSomeField() {
-//		return this.someField;
-//	}
-//	
-//	public <E> E someMethod(E t) {
-//		return null;
-//	}
-//	
-//}
-//
-//
-//class Parent  {
-//	
-//}
-//
-//class Child extends Parent {
-//	
-//}
-//
-//
-//
+
+}
+
+class Z<T extends Parent> {
+	
+	private T someField;
+	
+	public Z(T t) {
+		this.someField = t;
+	}
+	
+	public T getSomeField() {
+		return this.someField;
+	}
+	
+	public <E> E someMethod(E t) {
+		return null;
+	}
+	
+}
+
+
+class Parent  {
+	
+}
+
+class Child extends Parent {
+	
+}
+
+class Child2 extends Child {
+	
+}
+
+
+
--- src/io/NioDemo.java
+++ src/io/NioDemo.java
@@ -14,31 +14,27 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class NioDemo {
-	
+
 	public static void main(String[] args) throws IOException {
 		String pathToDirectory = "testDirectory" + File.separator + "tesSubDirectory";
 		Path path = Paths.get(pathToDirectory);
-		
+
 		Files.createDirectories(path);
-		
+
 		hm2();
-		
+
 		writeToFile();
 	}
-	
+
 	private static void hm() throws IOException {
 		Path start = Paths.get("");
 		int maxDepth = 4;
-		try (Stream<Path> stream = Files.find(start, maxDepth, (path, attr) ->
-		        String.valueOf(path).endsWith(".js"))) {
-		    String joined = stream
-		        .sorted()
-		        .map(String::valueOf)
-		        .collect(Collectors.joining("; "));
-		    System.out.println("Found: " + joined);
+		try (Stream<Path> stream = Files.find(start, maxDepth, (path, attr) -> String.valueOf(path).endsWith(".js"))) {
+			String joined = stream.sorted().map(String::valueOf).collect(Collectors.joining("; "));
+			System.out.println("Found: " + joined);
 		}
 	}
-	
+
 	private static void hm2() throws IOException {
 		Path start = Paths.get("");
 		int maxDepth = 3;
@@ -46,20 +42,24 @@ public class NioDemo {
 			return String.valueOf(path).endsWith(".java");
 		})) 
 		{
-		    String joined = stream
-		        .sorted()
-		        .map(String::valueOf)
-		        .collect(Collectors.joining("; "));
-		    System.out.println("Found: " + joined);
+			String joined = stream.sorted().map(String::valueOf).collect(Collectors.joining("; "));
+			System.out.println("Found: " + joined);
 		}
 	}
-	
+
 	private static void writeToFile() throws IOException {
 		List<String> lines = Arrays.asList("a", "s", "d");
 		Files.write(Paths.get("niotest.txt"), lines);
-		
+
 		String test = "asdasd";
-		Files.write(Paths.get("niotest2.txt"), test.getBytes(), StandardOpenOption.CREATE ,StandardOpenOption.APPEND);
+		Files.write(Paths.get("niotest2.txt"), test.getBytes(), StandardOpenOption.CREATE, StandardOpenOption.APPEND);
 	}
+	
+	
+//	Files.walk(Paths.get("/path/to/stuff/"))
+//    .filter(p -> p.toString().endsWith(".ext"))
+//    .map(p -> p.getParent().getParent())
+//    .distinct()
+//    .forEach(System.out::println);
 
 }
