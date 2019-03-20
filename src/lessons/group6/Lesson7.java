6db92b38b61cab3f890b1c11e13fe8c834f5b2d0
Andrii Piatakha <andrey.pyatakha@gmail.com>
2019-03-20 19:54:47 +0200

    commit

--- src/lessons/group6/Lesson7.java
+++ src/lessons/group6/Lesson7.java
@@ -1,6 +1,15 @@
 package lessons.group6;
 
+import java.util.Arrays;
+
 public class Lesson7 {
+	
+	public static void main(String[] args) {
+		String regex = ",|\n";
+		String s = "1,2*3*3\n";
+		String[] split = s.split("\\D");	
+		System.out.println(Arrays.toString(split));
+	}
 	
 	public int sum(Integer a, Integer b) {
 		if (a == null || b == null) {
