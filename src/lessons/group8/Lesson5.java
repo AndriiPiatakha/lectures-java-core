ce43dff573c1b2cc4481a62d00841e5b5f348c91
Andrii Piatakha <andrey.pyatakha@gmail.com>
2019-07-11 20:56:11 +0300

    commit

--- src/lessons/group8/Lesson5.java
+++ src/lessons/group8/Lesson5.java
@@ -1,5 +1,17 @@
 package lessons.group8;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class Lesson5 {
+	
+	public static void main(String[] args) {
+		String[] array = { "I ", "could ", "not ", "do", "it", "without", "list" };
+		String[] filteredArray = Arrays.stream(array)
+				.filter(x -> x.length() > 3)
+//				.collect(Collectors.toList());
+				.toArray(String[]::new);
+		
+	}
 
 }
